af27cacb826672e8eb6b89aec652d0c216a656ca
allan7yin <66652405+allan7yin@users.noreply.github.com>
2022-12-01 18:35:38 -0500

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,8 @@
 const request = require('request');
 
 const geocode = (address, callback) => {
-    const url = 'http://api.positionstack.com/v1/forward?access_key=33e8b2426cf20720443d24de143d57ac&query=' + encodeURIComponent(address);
+    const APIkey; // message me for this API key
+    const url = 'http://api.positionstack.com/v1/forward?access_key=' + APIKey + '&query=' + encodeURIComponent(address);
     request({url: url, json: true}, (error, response) => {
         // normally, we would do, if (error), but this API does not return an error if the address does not exist, so we just do data.length == 0
         if (response.body.data.length == 0) {
